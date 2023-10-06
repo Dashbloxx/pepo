ff2d642ecd7a0e3365c11af0e779aad45d9eb056
Dashbloxx <dashbloxx@gmail.com>
2023-10-05 22:30:08 -0400

    Working on feedforward function...

---
+++ source/feedforward.c
@@ -0,0 +1,9 @@
+#include "feedforward.h"
+#include "sigmoid.h"
+
+double feedforward(double input[], double weights_input_hidden[], double weights_hidden_output[])
+{
+    double output = 0.0;
+
+    return output;
+}
---
+++ source/feedforward.h
@@ -0,0 +1,3 @@
+#pragma once
+
+double feedforward(double input[], double weights_input_hidden[], double weights_hidden_output[]);
